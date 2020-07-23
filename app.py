5ac7988bd04ffcb7e0faa8ea863f430da6aa34a5
Md. Ahsanul Kabir Arif <61467255+makarif@users.noreply.github.com>
2020-07-24 00:24:57 +0600

    Update app.py

--- app.py
+++ app.py
@@ -5,9 +5,11 @@ import pickle
 app = Flask(__name__)
 model = pickle.load(open('model.pkl', 'rb'))
 
+
 @app.route('/')
 def home():
     return render_template('index.html')
+
 
 @app.route('/predict',methods=['POST'])
 def predict():
